f4243e0bd4cacc6f94e3bd36913b91833e2af4b1
RoadTTDream <85446131+RoadTTDream@users.noreply.github.com>
2021-06-08 11:32:16 +0700

    Delete preloader.js

--- VimeWorld_v2/scripts/preloader.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  const preload = document.querySelector('.preloader')
-  preload.id = 'hidePreload'
-  setTimeout(function () {
-    preload.classList.add('fullHide')
-  }, 950)
-}
-
